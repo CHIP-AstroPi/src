8b00e48afc980dd4b83d2898cb561041abed5c51
filippoferrando <ferrandodfilippo@gmail.com>
2020-11-27 18:36:04 +0100

    #test2 with flickr image

--- cloud_recon/cloud_recon.py
+++ cloud_recon/cloud_recon.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('/home/rdfilippo/Desktop/Scuola/AstroPi/src/train2.jpg', 1)
+cnt = 0;
+
+img = cv2.imread('/home/rdfilippo/Desktop/Scuola/AstroPi/src/cloud_recon/images/train14.jpg', 1)
 hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
 white = np.array([255,255,255])
@@ -10,7 +12,7 @@ lowerBound = np.array([30,30,30])
 mask = cv2.inRange(hsv, lowerBound, white)
 
 res = cv2.bitwise_and(img, img, mask=mask)
-cv2.imwrite("/home/rdfilippo/Desktop/Scuola/AstroPi/src/train2.jpg", res)
+cv2.imwrite("/home/rdfilippo/Desktop/Scuola/AstroPi/src/cloud_recon/images/train14-1.jpg", res)
 
 cv2.imshow("mywindow", res)
 
