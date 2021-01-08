f1095dbce7f0be64571c0d916c82b8ed5b8412eb
filippoferrando <ferrandodfilippo@gmail.com>
2021-01-09 00:48:03 +0100

    changed algorithm

--- cloud_recon/cloud_recon.py
+++ cloud_recon/cloud_recon.py
@@ -1,17 +1,42 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
+img = cv.imread('./images/train10.jpg')
+#img = img[:, 350:1500]
 
-def recon(file, img):
-    #img = cv2.imread(image, 1)
-    hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+plt.imshow(imRGB)
+plt.title('original')
+plt.show()
 
-    white = np.array([255,255,255])
-    lowerBound = np.array([30,30,30])
+#img = cv.GaussianBlur(img, (5,5), 0)
 
-    mask = cv2.inRange(hsv, lowerBound, white)
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-    res = cv2.bitwise_and(img, img, mask=mask)
-    cv2.imwrite(file, res)
+plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2HSV))
+plt.title('image')
+plt.show()
 
-    cv2.waitKey(0)
+white = np.array([255,255,255])
+lowerBound = np.array([30,10,30])
+
+mask = cv.inRange(hsv, lowerBound, white)
+
+res = cv.bitwise_and(img, img, mask=mask)
+
+plt.imshow(cv.bitwise_and(img, img, mask=mask))
+plt.title('elaborated')
+plt.show()
+
+#count of total pixel
+total = res.shape[0] * res.shape[1]
+print(f"total pixel: {total}")
+
+#count of black pixel
+sought = [0,0,0]
+black  = np.count_nonzero(np.all(res==sought,axis=2))
+print(f"black pixel: {black}")
+
+#perc of black pixels
+perc = (black*100)/total
+perc = "%.3f" % perc
+print(f"perc: {perc}%")
