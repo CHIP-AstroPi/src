07c6072c94373ffc96c8f870c24032dfb8628cd9
filippo-ferrando <filippo.ferrando@itiscuneo.eu>
2021-01-10 19:42:55 +0100

    working function

--- light_recon/light_condition_recon.py
+++ light_recon/light_condition_recon.py
@@ -1,46 +1,55 @@
-import cv2
+import cv2 as cv
 import numpy as np 
 
-def day_or_night(path,percentage):
-    MIN_SAVE = 85
-    img = cv2.imread(path)
-    #--------
+def is_day(img, size_percentage=30, min_threshold=70):
+
+
+    # Get image size
+    height, width, _ = img.shape
+
+    # Calculate center coordinate
+    centerX = (width // 2 )
+    centerY = (height // 2)
+
+    # Calculate RightBorder 
+    XRB = centerX + ((width * size_percentage) // 200)                    
+    # Calculate LeftBorder
+    XLB = centerX - ((width * size_percentage) // 200)
+    # Calculate TopBorder
+    YTB = centerY + ((height * size_percentage) // 200)
+    # Calculate BottomBorder
+    YBB = centerY - ((height * size_percentage) // 200)
 
     bgr_list = []
-    height,width,_ = img.shape
 
-    centerX = (width // 2 ) 
-    centerY = (height // 2)                                                                  
-
-    #RightBorder 
-    XRB = centerX + ((width * percentage)//200)                    
-    #LeftBorder
-    XLB = centerX - ((width * percentage)//200)
-    #TopBorder
-    YTB = centerY + ((height * percentage)//200)
-    #BottomBorder
-    YBB = centerY - ((height * percentage)//200)
-
-    #print(f"{height}, {width} xrb  {XRB} ---- xlb {XLB} ----- ytb {YTB} ----- ybb {YBB}") 
-
+    # Creation of a list of BGR values for every pixel
     for x in range(XLB,XRB):
         for y in range(YBB,YTB):
-            bgr_list.append(img[y,x])
+            bgr_list.append(img[y,x]) # Append the BGR value to the list
 
+    # Convert bgr_list in a numpy array
     numpy_bgr_array = np.array(bgr_list)
+    # Calculate the average value of blue, green and red
     average_value = np.average(numpy_bgr_array,axis=0)
 
-    average_value = average_value.astype(int)  #qui ho average-value
+    # Convert the type of datas
+    average_value = average_value.astype(int)
 
-    #----------------
-    #average_value = BGR_to_GRAY(average_value)
-    average_value = np.uint8([[[average_value[0],average_value[1],average_value[2]]]])
+    # Map values in uint8 format type
+    average_value = np.uint8([[[average_value[0],average_value[1],average_value[2]]]]) 
 
-    gray_avg_Values = cv2.cvtColor(average_value,cv2.COLOR_BGR2GRAY)
-    gray_avg_Values = np.squeeze(gray_avg_Values)               #qui ho il valore di grigio 
+    # Convert the color from BGR to Grayscale
+    gray_avg_value = cv.cvtColor(average_value, cv.COLOR_BGR2GRAY)
+    #remove single-dimensional entries from the shape of the numpy array
+    gray_avg_value = np.squeeze(gray_avg_value)
 
-    if gray_avg_Values >= MIN_SAVE:
-        #save image
-        pass
-    else:
-        pass
+    # Return True if the gray_avg value
+    return gray_avg_value >= min_threshold
+
+
+if __name__ == "__main__":
+    img = cv.imread('./images/train8.jpg')
+    img = img[:, 350:1500]
+
+    day = is_day(img)
+    print(f"è giorno: {day}")
