c189af0a18dafd6479517d7fc64d8cd887ced3ae
Pietro Jomini <pietro.jomini@protonmail.ch>
2021-01-26 13:23:14 +0100

    nocam updates > main

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ TODO check english (!)
 from pathlib import Path
 from picamera import PiCamera
 from picamera.array import PiRGBArray
+import numpy as np
 import cv2 as cv
 import logzero
 import logging
@@ -36,8 +37,8 @@ class Config():
 
     # runtime schedule
     # see the `runtime_schedule` function for more info
-    rs_step = 1  # [seconds]
-    rs_tot = 10  # [seconds]
+    rs_step = 2 * 60  # [seconds]
+    rs_tot = 15 * 60  # [seconds]
 
     # fs
     fs_here = Path(__file__).parent.resolve()
@@ -45,7 +46,7 @@ class Config():
     logfile_log = 'runtime.log'
 
     # camera
-    cam_resolution = (2592, 1944)
+    cam_resolution = (2592, 1952)
     cam_framerate = 15
     cam_format = "bgr"
 
@@ -58,8 +59,8 @@ class Config():
 
     # iss position
     iss_name = 'ISS (ZARYA)'
-    iss_l1 = '1 25544U 98067A   20016.35580316  .00000752  00000-0  21465-4 0  9996'
-    iss_l2 = '2 25544  51.6452  24.6741 0004961 136.6310 355.9024 15.49566400208322'
+    iss_l1 = '1 25544U 98067A   21026.29455175  .00001781  00000-0  40614-4 0  9995'
+    iss_l2 = '2 25544  51.6464 324.5349 0002308 292.1434 166.6739 15.48892138266603'
 
 
 # ----------------------------------------
@@ -190,6 +191,9 @@ def get_iss_data():
 # ----------------------------------------
 
 
+# ----------------------------------------
+# FUNCTIONS
+
 def runtime_scheduler(task: callable) -> None:
     """Handle runtime task scheduling
 
@@ -244,17 +248,149 @@ def runtime_scheduler(task: callable) -> None:
     logger.info(f'RS:end elapsed={round(time.time() - start, 3)}')
 
 
+def cut_image(img: np.ndarray, target_height_perc=65, target_width_perc=65) -> np.ndarray:
+    """Cut an image in a rectangle of size defined by parameters, to remove black borders"""
+
+    # retrieve image shape
+    old_width, old_height, _ = img.shape
+    half_width = old_width // 2
+    half_height = old_height // 2
+
+    # evaluate the needed padding on the x an y axis
+    padding_x = (half_width * target_height_perc) // 100
+    padding_y = (half_height * target_height_perc) // 100
+
+    # cut the image
+    return img[
+        half_width - padding_x:half_width + padding_x,
+        half_height - padding_y:half_height+padding_y
+    ]
+
+
+def is_day(img: np.ndarray, center_size_perc=30, threshold=70) -> bool:
+    """Check if the photo represent a daytime scenary
+
+    If the average brightness of the center of the image is higher than a `threshold`, 
+    it is assumed to be taken at daytime.
+    """
+
+    # retrieve image shape
+    height, width, _ = img.shape
+
+    # calculate center coordinate
+    center_x = width // 2
+    center_y = height // 2
+
+    # evaluates the dislocation of the more external sample points along the x and y axis
+    dislocation_x = (width * center_size_perc) // 100
+    dislocation_y = (height * center_size_perc) // 100
+
+    # finds the position of the more external sample points
+    border_left = center_x - dislocation_x
+    border_right = center_x + dislocation_x
+    border_bottom = center_y - dislocation_y
+    border_top = center_y + dislocation_y
+
+    # add all the sample points to a list
+    sample_points = []
+    for x in range(border_left, border_right):
+        for y in range(border_bottom, border_top):
+            sample_points.append(img[y, x])
+
+    # convert the sample points list in a numpy array
+    numpy_sample_points = np.array(sample_points)
+
+    # calculate the average value of the sample points
+    average_bgr = np.average(numpy_sample_points, axis=0)
+
+    # convert the bgr values to python ints
+    average_bgr = average_bgr.astype(int)
+
+    # convert the average value to opencv compliant format
+    average_bgr = np.uint8([[average_bgr]])
+
+    # convert the average color from bgr to grayscale
+    average_grayscale = cv.cvtColor(average_bgr, cv.COLOR_BGR2GRAY)
+
+    # retrieve the grayscale value
+    average_grayscale = np.squeeze(average_grayscale)
+
+    # compare the average grayscale value with the threshold
+    return average_grayscale >= threshold
+
+
+def cloud_percent(img: np.ndarray) -> float:
+    """Calculate the percentage of clouds in an image"""
+
+    # convert the image to hsv
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+    # extract the saturation of the image
+    saturation = hsv[:, :, 1]
+
+    # apply otsu threshold to the saturation
+    threshold_type = cv.THRESH_BINARY+cv.THRESH_OTSU
+    _, threshold = cv.threshold(saturation, 0, 255, threshold_type)
+
+    # find the contours of the threshold
+    contours = cv.findContours(
+        threshold,
+        cv.RETR_EXTERNAL,
+        cv.CHAIN_APPROX_NONE
+    )[-2]
+
+    # create an `img` clone
+    img_clone = np.copy(img)
+
+    # draw the contours on the img clone
+    cv.drawContours(img_clone, contours, -1, (0, 255, 0), 2)
+
+    # TODO what does it do?
+    x, y, w, h = cv.boundingRect(contours[0])
+    threshold[y:y+h, x:x+w] = 255 - threshold[y:y+h, x:x+w]
+    contours = cv.findContours(
+        threshold,
+        cv.RETR_EXTERNAL,
+        cv.CHAIN_APPROX_NONE
+    )[-2]
+
+    # fill contours
+    for contour in contours:
+        if cv.contourArea(contour) > 4:
+            cv.drawContours(
+                img_clone,
+                [contour],
+                -1,
+                (255, 0, 0),
+                thickness=cv.FILLED
+            )
+
+    # calculate the percentage of clouds
+    pixels_count = img_clone.shape[0] * img_clone.shape[1]
+    red_pixels = np.all(img_clone == [255, 0, 0], axis=2)
+    red_pixels_count = np.count_nonzero(red_pixels)
+    return 100 * red_pixels_count / pixels_count
+
+
+# /FUNCTIONS
+# ----------------------------------------
+
+
 # ----------------------------------------
 # MAIN
 
 
 def main():
-    img_id, image = camera_capture()
+    img_id, raw_image = camera_capture()
     img_path = image_path()
     iss_data = get_iss_data()
 
-    cv.imwrite(img_path, image)
-    log_data(img_id, img_path, *iss_data)
+    cutted_image = cut_image(raw_image)
+    if not is_day(cutted_image):
+        logger.info('Not daytime, closing')
+        
+    cv.imwrite(img_path, raw_image)
+    log_data(img_id, img_path, cloud_percent(cutted_image), *iss_data)
 
     camera_reset()
 
